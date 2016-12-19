4d57f1d34609ac5a86a615497b9379513b5e8c49
Mehul Parmar <mehul@Mehuls-MacBook-Pro.local>
2016-12-19 14:55:57 +0100

    Develop branch commit

---
+++ site/src/main/java/com/test/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.test.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="gitflowtest:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
